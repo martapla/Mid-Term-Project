ccaa5e2c0bd67a6633184405305f66d483d2c958
martapla <martaplanella@hotmail.com>
2023-01-26 22:55:20 +0100

    contact fetch-post

--- contact.js
+++ contact.js
@@ -1,32 +1,54 @@
 
+//Contact Post
+
+console.log("TESTCONTACT");
+
 const addPost = () => {
   
-    // nos traemos los values/valores de los inputs
-    let name = document.getElementById("text-name").value;
-    let email = document.getElementById("text-email").value;
-    let phone = document.getElementById("text-phone").value;
+// nos traemos los values/valores de los inputs
+let name = document.getElementById("name").value;
+// let email = document.getElementById("email").value;
+// let phone = document.getElementById("phone").value;
+let message = document.getElementById("message").value;
+  
+console.log(`Name: ${name} - Message: ${message} `);
+alert (`Thanks to Submit ${name} !`)
     
 
-    // Vamos a usar el fetch() para poder enviar info a el endpoint.
-    fetch("https://jsonplaceholder.typicode.com/users",
-        {
-      // Tipo de request que queremos ejecutar.
-      method: "POST",
+// Vamos a usar el fetch() para poder enviar info a el endpoint.
+fetch("https://jsonplaceholder.typicode.com/posts",
+{
+// Tipo de request que queremos ejecutar.
+method: "POST",
       
-      body: JSON.stringify({ name:text-name, email:text-email, phone:text-phone }),
-    })
-      .then((response) => response.json())
-      .then((formData) => console.log(formData))
-      .catch((error) => console.log(error));
+  body: JSON.stringify({
+    title: name,
+    body: message,
+    userId:1,
+
+  }),
+
+headers: {
+  "Content-type": "application/json; charset=UTF-8",
+  Accept: "text/plain, application/json",
+},
+
+})
+  
+.then((response) => response.json())
+.then((formData) => console.log(formData))
+.then(() => {
+    let formDone = document.querySelector(".s-form-done");
+    formDone.style.display = "block";
+})
+.catch((error) => console.log(error));
 };
   
-window.addEventListener("load", () => {
+// window.addEventListener("load", () => {
     
-    document.getElementById("submit-btn").addEventListener("click",() => {
-        addPost();
-    })
-})
-;
+// document.getElementById("submit-btn").addEventListener("click",() => { addPost() })
+// })
+// ;
 
 
-//document.querySelector("#addPost").addEventListener("submit", addPost);
+document.querySelector("#contactForm").addEventListener("submit", addPost);
