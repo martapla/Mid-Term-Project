d241ccf31589578474d8a0a4a25acd3fbe55134c
martapla <martaplanella@hotmail.com>
2023-01-29 18:50:08 +0100

    projects js done

--- projects.js
+++ projects.js
@@ -4,13 +4,14 @@ const apiJsonInfo = () => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((res) => {
-        let getApiInfo = "";
-        res.slice(0, 3).forEach((post, index) => {
+          let getApiInfo = "";
+          let newArray = res.slice(0, 3);
+          newArray.forEach((post, index) => {
           getApiInfo += `<div class="box">
               <div><img class="box img" src="./project-assets/projects-section/${ index + 1 }.jpg" alt=""></div>
               <div class="projects-text">
-                <h3 class="projects h3">${post.title.substring(0,15)}</h3>
-                <p class="projects p">${post.body.substring(0,100)}</p>
+                <h3 class="projects h3">${post.title.slice(0,1).toUpperCase()+ post.title.slice(1,15)}</h3>
+                <p class="projects p">${post.body.slice(0,1).toUpperCase()+ post.body.substring(1,100)}</p>
                 <a href="./projects.html">Learn More</a>
               </div>
           </div>`;
--- projects2.js
+++ projects2.js
@@ -1,49 +1,45 @@
-const getData = () => {
+//Fetch to : Title and Description 
+
+const getDataJson = () => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
         .then((res) => {
-            let project = res[1]
+            let project = res[0]
             console.log(project);
     
-             document.querySelector(".project-title").innerText =  project.title.substring(0,30)
-             document.querySelector(".project-text p ").innerText =  project.body
+            document.querySelector(".project-title").innerText = project.title.slice(0, 1).toUpperCase() + project.title.substring(1, 30);
+
+            document.querySelector(".project-text p ").innerText = project.body.slice(0, 1).toUpperCase() + project.body;
     
           })
     
       }
     
-getData()
+getDataJson()
       
 
-// const apiJsonInfo2 = () => {
-//     fetch("https://jsonplaceholder.typicode.com/posts")
-//       .then((response) => response.json())
-//       .then((res) => {
-
-
-//  let getApiInfo = "";
-//  res.slice(0,1).forEach((post) => {
-//      getApiInfo +=
-//          ` <div id="main-project">
-//            <div class="project-container">
-//              <h1 class="project-title">${post.title.substring(0, 15)}</h1>
-//              <div class="project-info">
-//              <div class="project-ui">UI Design & App Development</div>
-//              <div class="project-date">Completed on:  02/02/22. </div>
-//            </div>
-//            <div class="main-project">
-//                <img  src="../project-assets/projects-section/1.jpg" alt="square">
-//            </div>
-//            <div class="project-text">
-//                <p>${post.body}</p>
-//                <p>${post.body}</p>
-//            </div>
-//            </div>`;
+const getOtherProjects = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((res) => {
+          let getApiInfo = "";
+          //Other Projects Section run 1 position behind: Image (index +1) and Text with slice(1,4)
+          let newArray = res.slice(1, 4);
+           console.log(newArray);
+           newArray.forEach((post, index) => {
           
-//         });
-//         document.querySelector(".project-container").innerHTML = getApiInfo;
-//       })
-//       .catch((error) => console.log(error));
-//   };
+           getApiInfo += `<div class="box">
+               <div><img class="box img" src="./project-assets/projects-section/${ index + 2}.jpg" alt=""></div>
+               <div class="projects-text">
+                <h3 class="projects h3">${post.title.slice(0,1).toUpperCase()+ post.title.slice(1,15)}</h3>
+                <p class="projects p">${post.body.slice(0,1).toUpperCase()+ post.body.substring(1,100)}</p>
+                <a href="./projects.html">Learn More</a>
+              </div>
+            </div>`;
+        });
+        document.querySelector(".collection-box").innerHTML = getApiInfo;
+      })
+      .catch((error) => console.log(error));
+  };
   
-//   apiJsonInfo2();
+  getOtherProjects();
